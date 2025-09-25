8f324dcc7b6158fe382ddc2b5a12dbb2a34ff3a5
MathuBharathi <mathubharathi15@gmail.com>
2025-09-25 15:25:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 10000; // Render provides PORT automatically
+
+app.get("/", (req, res) => {
+  res.send("Hello from Render!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
